a123c768c48957718d0f6959681d9af96dd90ec7
brivan1 <bkryan.bk@gmail.com>
2024-05-13 12:01:45 +0300

    package updates

--- MajiApp/majiapp_main.py
+++ MajiApp/majiapp_main.py
@@ -3,20 +3,23 @@ import MySQLdb
 #define class User
 
 class User():
-    def __init__(self, username, password):
+    def __init__(self, username, email):
         self.username = username
-        self.password = password
+        self.email = email
 class Storage():
     def __init__(self):
         self.connection = MySQLdb.connect(host='localhost', user='MajiApp', password='B@Unix', db='MajiApp_db')
         self.cursor = self.connection.cursor()
         self.users = []
-    def add_user(self, username, password):
-        new_user = User(username, password)
+    def add_user(self, username, email):
+        new_user = User(username, email)
         self.users.append(new_user)
+        query = "INSERT INTO User_maji (username, email) VALUES (%s, %s)"
+        self.cursor.execute(query, (username, email))
+        self.connection.commit()
     def list_users(self):
         for user in self.users:
-            print(f"User: {user.username} ({user.password})")
+            print(f"User: {user.username} ({user.email})")
         
 class  MajiAppCLI(cmd.Cmd):
     def __init__(self):
@@ -26,12 +29,12 @@ class  MajiAppCLI(cmd.Cmd):
       
     def do_add_user(self, arg):
         args = arg.split()
-        if len(args) == 2:
-            username = args[0]
-            password = args[1]
-            storage = Storage()
-            storage.add_user(username, password)
-            print(f"user {username} added")
+        if len(args) != 2:
+            print("Usage: add_user <username> <email>")
+            return
+        username, email = args
+        storage.add_user(username, email)
+        print(f"user {username} added")
     def do_list_users(self, arg):
         storage.list_users()
     def do_exit(self, arg):
