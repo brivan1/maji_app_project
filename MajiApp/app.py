0f13d443dd36408a2ea145af0cdf2aad3dba9aea
brivan1 <bkryan.bk@gmail.com>
2024-05-19 23:25:00 +0300

    more changes

--- MajiApp/app.py
+++ MajiApp/app.py
@@ -1,44 +1,58 @@
-from flask import Flask, request, render_template, session, redirect, url_for
-from flask import MySQLdb, MySQL
+from flask import Flask, request, render_template, session, redirect
+# from majiapp_main import Storage
+# from flask_mysqlbd import MySQLdb
+from MySQLdb.cursors import DictCursor
+from flask_mysqldb import MySQL
 
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.secret_key = "MajiApp_secret_key"
+
 app.config['MySQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'MajiApp'
 app.config['MYSQL_PASSWORD'] = 'B@Unix'
 app.config['MYSQL_DB'] = 'MajiApp_db'
+
 mysql = MySQL(app)
     
+# @app.route('/')
+# def index():
+#     return "hello"
+# @app.route('/login',methods=['GET','POST'])
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+
 @app.route('/login', methods=['GET','POST'])
-def login(username, password):
+def login():
     
-    
+
     if request.method == "POST" and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
-        password = request.form['password']
-        cursor = MySQLdb.connect(host=app.config['MYSQL_HOST'], user=app.config['MYSQL_USER'], password=app.config['MYSQL_PASSWORD'], db=app.config['MYSQL_DB']).cursor(MySQLdb.cursors.DictCursor)
+        password = request.form['password'] 
+        cursor = mysql.connection.cursor(DictCursor)
         cursor.execute("SELECT * FROM User_maji WHERE username = %s AND password = %s", (username, password))
-        account = cursor.fetchone()
-        if account:
-            session['logged_in'] = True
-            session['id'] = account['id']
-            session['username'] = username
-            return render_template('index.html')
-        else:
-            return render_template('login.html', info="invalid password!")
-    
+        user = cursor.fetchone()
 
+        if user:
+            session['logged_in'] = True
+            session['username'] = ['username']
+            session['password'] = ['password']
+            return redirect('/dashboard.html', info="welcome to MajiApp")
+        else:
+            return render_template('login.html', info="invalid password or username!")
+        
+    
     return render_template('login.html')
 
-@app.route('/logout')
-def logout():
+# @app.route('/logout')
+# def logout():
     
-    session.pop('loggedin', None)
-    session.pop('username', None)
-    session.pop('id', None)
-    return redirect(url_for('login'))
+#     session.pop('loggedin', None)
+#     session.pop('username', None)
+#     session.pop('id', None)
+#     return redirect(url_for('login'))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', debug=True)
--- MajiApp/majiapp_main.py
+++ MajiApp/majiapp_main.py
@@ -1,25 +1,33 @@
 import cmd
-from flask import MySQLdb
+import MySQLdb
 #define class User
 
 class User():
-    def __init__(self, username, email):
+    def __init__(self, username, password, complaints):
         self.username = username
-        self.email = email
+        self.password = password
+        # self.complaints = complaints[]
 class Storage():
     def __init__(self):
         self.connection = MySQLdb.connect(host='localhost', user='MajiApp', password='B@Unix', db='MajiApp_db')
         self.cursor = self.connection.cursor()
         self.users = []
-    def add_user(self, username, email):
-        new_user = User(username, email)
+        self.users = self.fetch_users()
+    def fetch_users(self):
+        query = "SELECT * FROM User_maji"
+        self.cursor.execute(query)
+        users = self.cursor.fetchall()
+        for user in users:
+            self.users.append(User(user['username'], user['password']))
+    def add_user(self, username, password):
+        new_user = User(username, password)
         self.users.append(new_user)
-        query = "INSERT INTO User_maji (username, email) VALUES (%s, %s)"
-        self.cursor.execute(query, (username, email))
+        query = "INSERT INTO User_maji (username, password) VALUES (%s, %s)"
+        self.cursor.execute(query, (username, password))
         self.connection.commit()
     def list_users(self):
         for user in self.users:
-            print(f"User: {user.username} ({user.email})")
+            print(f"User: {user.username} ({user.password})")
         
 class  MajiAppCLI(cmd.Cmd):
     def __init__(self):
@@ -32,8 +40,8 @@ class  MajiAppCLI(cmd.Cmd):
         if len(args) != 2:
             print("Usage: add_user <username> <email>")
             return
-        username, email = args
-        storage.add_user(username, email)
+        username, password = args
+        storage.add_user(username, password)
         print(f"user {username} added")
     def do_list_users(self, arg):
         storage.list_users()
