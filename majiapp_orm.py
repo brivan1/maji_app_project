46a81532d9e1e680f546565d53f6ca9067cc127d
brivan1 <bkryan.bk@gmail.com>
2024-06-17 00:57:56 +0300

    majiapp_orm.py changes

--- majiapp_orm.py
+++ majiapp_orm.py
@@ -1,16 +1,17 @@
 import cmd
-from sqlalchemy import Column, Integer, String, ForeignKey 
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine 
+from sqlalchemy.orm import relationship, sessionmaker, declarative_base
+
+Base = declarative_base()
 
 #Define a User class
-class User:
+class User(Base):
     
-    __tablename__ = 'Orm_maji'
+    __tablename__ = 'Orm_user'
     id = Column(Integer, primary_key=True)
-    username = Column(String(50))
-    email = Column(String(100))
-    
-    
+    username = Column(String(20))
+    email = Column(String(20))
+    commplaints = relationship('Complaints', back_populates='user')
     
     '''def __init__(self, username, email, location):
         self.username = username
@@ -21,11 +22,14 @@ class User:
     def add_complaint(self, complaint):
         self.complaints.append(complaint)
 
-class Complaints:
+class Complaints(Base):
     
-    __tablename__ = 'Complaints'
+    __tablename__ = 'Orm_complaints'
     
     id = Column(Integer, primary_key=True)
+    message = Column(String(200))
+    user_id = Column(Integer, ForeignKey('Orm_user.id'))
+    user = relationship('User', back_populates='complaints')
     
     """
     def __init__(self, username, message):
@@ -35,19 +39,39 @@ class Complaints:
     
 class Storage:
     def __init__(self):
-        self.users = []
+        
+        path = 'mysql+pymysql://MajiApp:B@Unix:@localhost:orm_maji'
+        self.engine = create_engine(path)
+        
+        Base.metadata.create_all(self.engine)
+        self.Session = sessionmaker(bind=self.engine)
+        self.session = self.Session()
+        
+        # self.users = []
         
     def add_user(self, username, email, location):
-        new_user = User(username, email, location)
-        self.users.append(new_user)
+        new_user = User(username=username, email=email, location=location)
+        self.session.add(new_user)
+        self.session.commit()
+        # self.users.append(new_user)
         
     def add_complaints(self, username, message):
+        user = self.session.query(User).filter_by(username=username).first()
+        if user:
+            complaint = Complaints(message=message, user=user)
+            self.session.add(complaint)
+            self.session.commit()       
+       
+        '''
         for user in self.users:
             if user.username == username:
                 complaint = Complaints(message)
                 user.add_complaints(complaint)
+        '''
+                
+                
     def list_users(self):
-        for user in self.users:
+        for user in self.session.query(User).all():
             print(f"User:{user.username}")
             for complaint in user.complaints:
                 print(f"Complaints: {complaint.message}")
